f8d543c1fb888d5d22207ea8e25644c0a7e9d18e
vmathur <vmathur@43700839-f8e2-0310-9689-8ed4146226e7>
2004-03-22 17:37:26 +0000

    Modified the enrollment script to accept a list of OSP Servers and enroll with them. Also modified the enrollment script for improved error reporting.
    git-svn-id: https://svn.code.sf.net/p/osp-toolkit/code/main@271 43700839-f8e2-0310-9689-8ed4146226e7

--- bin/enroll.sh
+++ bin/enroll.sh
@@ -11,8 +11,8 @@ OPENSSL_CONF=./openssl.cnf
 export OPENSSL_CONF
 RANDFILE=./.rnd
 export RANDFILE
-if [ $# -ne 1 ]; then
-echo "Usage: <script name> <osp server ip address>"
+if [ $# -le 0 ]; then
+echo "Usage: <script name> <list of osp server ip addresses>"
 exit 127
 fi
 
@@ -26,16 +26,16 @@ echo "Request Failed"
 exit 127
 fi
 
-enroll -function getcacert -caurl http://$1:1080/tep > cacert.pem
+enroll -function getcacert -caurl http://$1:1080/tep > cacert_0.pem
 ret_val=$?
 export ret_val
-echo "Error Code returned from getcacert command :" $ret_val
+echo "[SP: $1]Error Code returned from getcacert command :" $ret_val
 if [ "$ret_val" != "0" ]; then
 echo "Request Failed"
 exit 127
 fi
 
-enroll -function request -username trans -password nexus -customer 1000 -device 1000 -cacert cacert.pem -certreq certreq.pem -sslurl https://$1:1443/tep > localcert.pem
+enroll -function request -username trans -password nexus -customer 1000 -device 1000 -cacert cacert_0.pem -certreq certreq.pem -sslurl https://$1:1443/tep > localcert.pem
 ret_val=$?
 export ret_val
 echo "Error Code returned from localcert command :" $ret_val
@@ -43,3 +43,18 @@ if [ "$ret_val" != "0" ]; then
 echo "Request Failed"
 exit 127
 fi
+
+i=1
+shift
+while [ $# -ge 1 ]; do
+enroll -function getcacert -caurl http://$1:1080/tep > cacert_$i.pem
+ret_val=$?
+export ret_val
+echo "[SP: $1]Error Code returned from getcacert command :" $ret_val
+if [ "$ret_val" != "0" ]; then
+echo "Request Failed"
+exit 127
+fi
+i=`expr $i + 1`
+shift
+done
