962c599532d8fd4b47158f94f0b6841d7f55476f
siposp <siposp@43700839-f8e2-0310-9689-8ed4146226e7>
2004-12-10 21:45:25 +0000

    Updated Build script.
    git-svn-id: https://svn.code.sf.net/p/osp-toolkit/code/main@382 43700839-f8e2-0310-9689-8ed4146226e7

--- BUILD_RELEASE.sh
+++ BUILD_RELEASE.sh
@@ -17,7 +17,7 @@ ZIP_OPTIONS=-f
 ##	Destribution File Name
 ##
 VERSION=$1
-DESTRIBUTION_NAME="osptoolkit/BUILDs/OSPToolkit-""$VERSION"".tar"
+DISTRIBUTION_NAME="./TK-3_3_0-20041210/BUILDs/OSPToolkit-""$VERSION"".tar"
 
 ##
 ##	Remove old files
@@ -30,33 +30,33 @@ rm -f *.tar *.gz
 LIST_OF_EXCLUDED_FILES="exclude_while_taring"
 
 # Copy openssl.cnf file
-cp $CVSENVROOT/vendor_libs/openssl/apps/openssl.cnf bin/
+#cp $CVSENVROOT/vendor_libs/openssl/apps/openssl.cnf bin/
 
 # CVS dirs
 cd ..
-find osptoolkit -name CVS -type dir | cut -b 1-   > $LIST_OF_EXCLUDED_FILES
+find ./TK-3_3_0-20041210 -name ".svn" -type d | cut -b 1-   > $LIST_OF_EXCLUDED_FILES
 
 # This script
-echo "osptoolkit/"$0                   		>> $LIST_OF_EXCLUDED_FILES
+echo "TK*/"$0                   		>> $LIST_OF_EXCLUDED_FILES
 
 # The list itself
 echo $LIST_OF_EXCLUDED_FILES	>> $LIST_OF_EXCLUDED_FILES
-echo "osptoolkit/BUILDs"									>> $LIST_OF_EXCLUDED_FILES
+echo "./TK-3_3_0-20041210/BUILDs"									>> $LIST_OF_EXCLUDED_FILES
 
 # Do NOT include documentation folder
-echo "osptoolkit/doc"										>> $LIST_OF_EXCLUDED_FILES
+echo "./TK-3_3_0-20041210/doc"										>> $LIST_OF_EXCLUDED_FILES
 
 
 
 ##
 ##	Tar it
 ##
-$TAR $TAR_OPTIONS $DESTRIBUTION_NAME $LIST_OF_EXCLUDED_FILES osptoolkit/*
+$TAR $TAR_OPTIONS $DISTRIBUTION_NAME $LIST_OF_EXCLUDED_FILES ./TK-3_3_0-20041210/*
 
 ##
 ##	Zip it
 ##
-$ZIP $ZIP_OPTIONS $DESTRIBUTION_NAME
+$ZIP $ZIP_OPTIONS $DISTRIBUTION_NAME
 
 ##
 ##	Clean Up
