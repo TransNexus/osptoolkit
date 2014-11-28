6b467e009da4c1362fe9adb45481cf40f15237d6
di-shi <di-shi@43700839-f8e2-0310-9689-8ed4146226e7>
2014-11-28 01:35:57 +0000

    Added vendor Cisco.
    git-svn-id: https://svn.code.sf.net/p/osp-toolkit/code/branches/4.4@705 43700839-f8e2-0310-9689-8ed4146226e7

--- src/ospmsgattr.c
+++ src/ospmsgattr.c
@@ -82,6 +82,7 @@ const OSPT_MSG_DESC OSPV_RVENDOR_DESCS[OSPC_RVENDOR_NUMBER] = {
     { OSPC_RVENDOR_ASTERISK,    "asterisk" },
     { OSPC_RVENDOR_AUDIOCODES,  "audiocodes" },
     { OSPC_RVENDOR_BROADWORKS,  "broadworks" },
+    { OSPC_RVENDOR_CISCO,       "cisco" },
     { OSPC_RVENDOR_CUCM,        "cucm" },
     { OSPC_RVENDOR_FREESWITCH,  "freeswitch" },
     { OSPC_RVENDOR_GENBANDS3,   "genbands3" },
